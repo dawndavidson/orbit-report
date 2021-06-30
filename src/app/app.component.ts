748fd2222f2dc4854fd2bca66e85f3b33f7a52d8
dawndavidson <84029532+dawndavidson@users.noreply.github.com>
2021-06-29 19:44:48 -0500

    finally

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,24 +12,30 @@ export class AppComponent {
   sourceList: Satellite[];
   constructor() {
     this.sourceList = [];
-    
-    
     let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
  
     window.fetch(satellitesUrl).then(function(response) {
        response.json().then(function(data) {
+         
+ 
           let fetchedSatellites = data.satellites;
-          // TODO: loop over satellites
-          for(let i=0; i <fetchedSatellites.length; i++) {
-            new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
-          }
-          // TODO: create a Satellite object using new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
+         
           
-          this.sourceList.push(satellite);
+          for(let i=0; i <fetchedSatellites.length; i++){
+           let satellite = new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
+             
+              this.sourceList.push(satellite);      
+            
+          }
+          
+          // TODO: loop over satellites
+          // TODO: create a Satellite object using new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
           // TODO: add the new Satellite object to sourceList using: this.sourceList.push(satellite);
- console.log(Satellite);
+ 
        }.bind(this));
     }.bind(this));
- 
+   
  }
 }
+         
+      
