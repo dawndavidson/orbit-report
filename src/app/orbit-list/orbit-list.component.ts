5d7641754dc0f21ec58966e6e6b3c085cdc06ed7
dawndavidson <84029532+dawndavidson@users.noreply.github.com>
2021-07-02 11:24:43 -0500

    still missing sort & display

--- src/app/orbit-list/orbit-list.component.ts
+++ src/app/orbit-list/orbit-list.component.ts
@@ -12,6 +12,7 @@ export class OrbitListComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+     
   }
   sort(column: string): void {
     // array.sort modifies the array, sorting the items based on the given compare function
